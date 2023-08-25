9ee61dd4c8fd13012aa2adc41f7e6d58022be16e
Philip Blyth <nemoDreamer@users.noreply.github.com>
2023-08-24 23:30:09 -0400

    build: switch to nodemon for watch-mode

--- src/build.js
+++ src/build.js
@@ -1,30 +1,20 @@
 #! /usr/bin/env node
 
-const chokidar = require("chokidar");
-const fresh = require("fresh-require");
 const fs = require("fs");
 const log = require("npmlog");
 const path = require("path");
 const yargs = require("yargs");
 
-// Theme configs
-const themeConfigs = {
-  dark: {
-    name: "SeaShells (Dark)",
-    type: "dark",
-    scheme: "./schemes/seashells-dark",
-  },
-};
+const makeColorTheme = require("./functions/makeColorTheme");
+
+const darkScheme = require("./schemes/seashells-dark");
+
+// Schemes
+const schemes = [darkScheme];
 
 // ---
 
 const args = yargs
-  .option("watch", {
-    alias: "w",
-    description: "Re-build on file changes.",
-    type: "boolean",
-    default: false,
-  })
   .option("log-level", {
     alias: "l",
     type: "string",
@@ -44,22 +34,14 @@ const relative = (to) => `./${path.relative(ROOT, to)}`;
 
 // ---
 
-const build = () => {
-  Object.entries(themeConfigs).forEach(([themeId, themeConfig]) => {
-    log.info("Processing theme id:", themeId);
+(() => {
+  schemes.forEach((scheme) => {
+    log.info("Processing theme id:", scheme.id);
 
-    // refresh imports (because of watch-mode)
-    const scheme = fresh(themeConfig.path, require);
-    const makeColorTheme = fresh("./functions/makeColorTheme", require);
-
-    const theme = makeColorTheme({
-      ...themeConfig,
-      scheme,
-    });
-
+    const theme = makeColorTheme(scheme);
     const json = JSON.stringify(theme, null, 2);
 
-    const filename = `seashells-${themeId}-color-theme.json`;
+    const filename = `seashells-${scheme.id}-color-theme.json`;
     const filepath = path.resolve(ROOT, "themes", filename);
 
     fs.writeFileSync(filepath, json);
@@ -68,18 +50,4 @@ const build = () => {
   });
 
   log.info("👍 Done!");
-};
-
-if (args.watch) {
-  const files = path.resolve(ROOT, "src");
-
-  log.info("Watching files", relative(files));
-
-  chokidar
-    .watch(files, {
-      ignoreInitial: true,
-    })
-    .on("all", build);
-} else {
-  build();
-}
+})();
