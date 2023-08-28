6bc672ad27530c22b53343c577d0aa5a0d76d6f8
Philip Blyth <nemoDreamer@users.noreply.github.com>
2023-08-28 09:29:43 -0400

    build(utils): add invert, invertLuminance, processColors

--- src/functions/__tests__/utils.spec.js
+++ src/functions/__tests__/utils.spec.js
@@ -1,4 +1,10 @@
-const { assignVariations } = require("../utils");
+const seashellsDark = require("../../schemes/seashells-dark");
+const {
+  assignVariations,
+  processColors,
+  invert,
+  invertLuminance,
+} = require("../utils");
 
 describe("utils", () => {
   test("assignVariations", () => {
@@ -38,5 +44,67 @@ describe("utils", () => {
         "foreground": "#cccccc",
       }
     `);
+  });
+
+  const colors = {
+    black: "#000",
+    white: "white",
+    red: "#ff0000",
+    green: "#80ff80",
+  };
+
+  describe("processColors", () => {
+    test("dummy", () => {
+      expect(processColors(colors, () => "yo")).toMatchInlineSnapshot(`
+        {
+          "black": "yo",
+          "green": "yo",
+          "red": "yo",
+          "white": "yo",
+        }
+      `);
+    });
+
+    test("scheme", () => {
+      expect(processColors(seashellsDark.scheme, invert))
+        .toMatchInlineSnapshot(`
+        {
+          "ansi": {
+            "black": "#d5bda4",
+            "blue": "#cca7a0",
+            "brightBlack": "#aea69d",
+            "brightBlue": "#73321c",
+            "brightCyan": "#60453f",
+            "brightGreen": "#816157",
+            "brightMagenta": "#311a13",
+            "brightRed": "#006275",
+            "brightWhite": "#001a2a",
+            "brightYellow": "#00274d",
+            "cyan": "#7d4b3d",
+            "green": "#a47052",
+            "magenta": "#55210e",
+            "red": "#008bc9",
+            "white": "#003c5f",
+            "yellow": "#0050a9",
+          },
+          "background": "#f7ede3",
+          "bold": "#001a2b",
+          "brightForeground": "#001a2a",
+          "cursorText": "#0050a9",
+          "foreground": "#003c5f",
+          "links": "#939622",
+          "selectedText": "#001a2a",
+          "selection": "#c5a88b",
+        }
+      `);
+    });
+  });
+
+  test("invert", () => {
+    expect(invert("#f00")).toBe("#008fe0");
+  });
+
+  test("invertLuminance", () => {
+    expect(invertLuminance("#f00")).toBe("#e90000");
   });
 });
--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,6 +1,29 @@
 const chroma = require("chroma-js");
 
+/**
+ * @param {String} hexColor
+ * @param {String} hexAlpha
+ * @returns {String}
+ */
 const alpha = (hexColor, hexAlpha) => `${hexColor}${hexAlpha}`;
+
+/**
+ * @param {String} hexColor
+ * @returns {String}
+ */
+const invert = (hexColor) => {
+  const [l, c, h] = chroma(hexColor).lch();
+  return chroma.lch([100 - l, c, (h + 180) % 360]).hex();
+};
+
+/**
+ * @param {String} hexColor
+ * @returns {String}
+ */
+const invertLuminance = (hexColor) => {
+  const [l, c, h] = chroma(hexColor).lch();
+  return chroma.lch([100 - l, c, h]).hex();
+};
 
 const assignVariations = (obj) => {
   Object.entries(obj)
@@ -33,7 +56,28 @@ const assignVariations = (obj) => {
     });
 };
 
+/**
+ * @param {String|Object} scheme
+ * @returns {String|Object}
+ */
+const processColors = (scheme, callback) => {
+  if (typeof scheme === "string") {
+    return callback(scheme);
+  }
+
+  return Object.entries(scheme).reduce(
+    (prev, [key, value]) => ({
+      ...prev,
+      [key]: processColors(value, callback),
+    }),
+    {}
+  );
+};
+
 module.exports = {
   alpha,
+  invert,
+  invertLuminance,
   assignVariations,
+  processColors,
 };
